a5c3967251ea62a779d5e955846b2dda2387738f
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-03-09 12:48:40 +0100

    Adding 2 exercises

---
+++ ex1/exercise.js
@@ -0,0 +1,33 @@
+// var object = {
+//     id: 1,
+//     title: "this is a note",
+//     content: "This is the amazing content of the note!"
+//   }
+  
+//   Take the object, add one key-value pair to it and transform it in a JSON.
+  
+//   Now take the JSON, transform it back into an object with a new name, call and console log each one of the values.
+
+var object = {
+    id: 1,
+    title: "this is a note",
+    content: "This is the amazing content of the note!"
+
+}
+////////////////////////////////////////////////////////////////////////////
+object.name = "Daniel";
+
+console.log(object.name + " with ID:" +object.id);
+/////////////////////////////////////////////////////////////////////
+let myJSON = JSON.stringify(object);
+
+console.log(myJSON);
+//////////////////////////////////////////////////////////////////////
+var object2 = JSON.parse(myJSON)
+
+console.log(object2)
+////////////////////////////////////////////////////////////////////////////////////
+for (var all in object2){  //loggin all properties
+    console.log(`${all}: ${object2[all]}`)
+}
+
